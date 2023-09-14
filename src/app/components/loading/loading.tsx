453944d16b4aec30b909d81d455d75914c200051
Eduardo Cerutti <ceruttioliveira@hotmail.com>
2023-09-14 14:36:40 -0300

    Try progress bar with Three FIber Drei useProgress hook

--- src/app/components/loading/loading.tsx
+++ src/app/components/loading/loading.tsx
@@ -1,17 +1,9 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { useProgress } from '@react-three/drei'
 
 export default function Loading() {
-  const LOADING_TIME = 6000
-
-  const [progressBar, setProgressBar] = useState(0)
-
-  useEffect(() => {
-    setTimeout(() => {
-      progressBar < 100 && setProgressBar(progressBar + 1)
-    }, LOADING_TIME / 100)
-  }, [progressBar, LOADING_TIME])
+  const { progress } = useProgress()
 
   return (
     <div className="flex flex-col justify-center items-center h-screen gap-6">
@@ -21,12 +13,12 @@ export default function Loading() {
       </h3>
       <div className="flex justify-center items-center">
         <div className="flex justify-center absolute w-96">
-          <span>{`${progressBar}%`}</span>
+          <span>{`${progress}%`}</span>
         </div>
         <div className="flex bg-slate-600 rounded w-96 h-7">
           <div
             className="bg-cyan-500 rounded"
-            style={{ width: `${progressBar}%` }}
+            style={{ width: `${progress}%` }}
           />
         </div>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,15 @@
 'use client'
 
 import { Canvas } from '@react-three/fiber'
-import { Suspense, useEffect, useLayoutEffect, useState } from 'react'
+import { Suspense, useLayoutEffect, useState } from 'react'
 import Earth from './components/earth/earth'
 import Universe from './components/universe/universe'
 import Loading from './components/loading/loading'
 
 export default function Home() {
   const DESKTOP_MIN_WIDTH = 1280
-  const LOADING_TIME = 6000
 
   const [isMobile, setIsMobile] = useState<boolean>()
-  const [isLoading, setIsLoading] = useState(true)
-
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false)
-    }, LOADING_TIME)
-  }, [])
 
   useLayoutEffect(() => {
     setIsMobile(window.innerWidth < DESKTOP_MIN_WIDTH ? true : false)
@@ -25,16 +17,13 @@ export default function Home() {
 
   return (
     <div className="h-screen bg-gray-950">
-      {isLoading && <Loading />}
-      <Canvas
-        style={isLoading ? { display: 'none' } : { display: 'flex' }}
-      >
-        <Suspense>
+      <Suspense fallback={<Loading />}>
+        <Canvas>
           <Universe isMobile={isMobile!}>
             <Earth isMobile={isMobile!} />
           </Universe>
-        </Suspense>
-      </Canvas>
+        </Canvas>
+      </Suspense>
     </div>
   )
 }
